73d18698287bc45e077c413d0a2eb3b073470dff
diglean <diegoleandro2002@gmail.com>
2024-01-19 13:18:46 -0300

    Creating API

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -5,7 +5,9 @@ function Select ({ text, name, options, handleOnChange, value }) {
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
-                <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.name}>{option.name}</option>
+                ))}
             </select>
         </div>
     )
